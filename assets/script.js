5d15bd8ca8023a8cf6a68f26df5bab52ae227939
manraj <mkooner@live.com>
2024-04-24 08:22:34 +0100

    Add basic functionality and resize-slider

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,73 @@
-const GRID_LENGTH = 16;
+let GRID_LENGTH = 64;
+const PADDING_RATIO = 3;
+const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+let paddingHeight = `${Math.round((viewportHeight/PADDING_RATIO)/GRID_LENGTH)}px`;
+const gridContainerLength = (viewportHeight - paddingHeight)+ "px";
 
+//-----------------------------------------------------------
+// all slider values
+const slider = document.querySelector(".slider");
+const sliderText = document.querySelector(".slider-container > p");
+
+gridSlider.addEventListener("input", () =>{
+    sliderValue = parseInt(gridSlider.value);
+    redrawBoard(sliderValue);
+    
+});
+function updateSlider(){
+    sliderText.textContent = `Resolution: (${GRID_LENGTH}x${GRID_LENGTH})`;
+}
+//-----------------------------------------------------------
+//bounds within with grid will live
 const gridContainer = document.querySelector(".grid-container");
+gridContainer.style.height = gridContainerLength;
+gridContainer.style.width = gridContainerLength;
+//-----------------------------------------------------------
+// to allow the user to choose when they want to draw on the board
+let mouseDown = false;
+document.addEventListener("mousedown", () => mouseDown = true );
+document.addEventListener("mouseup", () => mouseDown = false );
 
-const grid = new Array(GRID_LENGTH);
+gridContainer.addEventListener("mouseover", (event) => {
+    if (mouseDown && event.target.classList.contains("square")) {
+        event.target.style.backgroundColor = "black";
+    }
+});
+//-----------------------------------------------------------
+// create a 2d array within grid container to house the board
+function drawBoard(GRID_LENGTH){
 for(let i=0; i<GRID_LENGTH; ++i) {
     const rowSquare = document.createElement("div");
-    rowSquare.id = `row-${i}`
+    rowSquare.classList.add("row");
+
     for(let j =0; j<GRID_LENGTH;++j) {
         const gridSquare = document.createElement("div");
-        gridSquare.id = `square-(${i},${j})`;
-        gridSquare.textContent=`(${i},${j})`;
-        console.log(`${i}`);
+        gridSquare.classList.add("square");
+        gridSquare.style.padding = paddingHeight;
         rowSquare.appendChild(gridSquare);
     }
+
     gridContainer.appendChild(rowSquare);
 }
+}
+//-----------------------------------------------------------
+function clearBoard(){
+    gridContainer.innerHTML = "";
+}
+
+//-----------------------------------------------------------
+function updatePadding(){
+    paddingHeight = `${((viewportHeight/PADDING_RATIO)/GRID_LENGTH)}px`;
+}
+function redrawBoard(input){
+    GRID_LENGTH = input;
+    clearBoard();
+    updatePadding();
+    drawBoard(input);
+    updateSlider();
+}
+//-----------------------------------------------------------
+//int main(){
+    redrawBoard(GRID_LENGTH);
+//  return 0;
+//}
